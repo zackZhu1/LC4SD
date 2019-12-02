3776469059731b09219c443fffc8aed30738b750
zackzhu1 <zackzhu1@gmail.com>
2019-12-02 10:36:18 -0800

    LC1064

---
+++ BinarySearch/LC1064.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fixedPoint(int[] A) {
+        // sanity check
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = A.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] == mid) {
+                right = mid;
+            } else if (A[mid] < mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return A[left] == left ? left : -1;
+    }
+}
